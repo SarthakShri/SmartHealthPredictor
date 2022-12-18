90b16a26bdb76e97d5f1d8cd54c19cfb5d54da7b
Sarthak Shrivastava <72563740+SarthakShri@users.noreply.github.com>
2022-12-18 14:38:29 +0530

    Update MLProjModel.py

--- MLProjModel.py
+++ MLProjModel.py
@@ -13,11 +13,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('/ML Project/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('/ML Project/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('/ML Project/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
